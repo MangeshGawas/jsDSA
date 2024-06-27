87c4c945038ec9b53e752af1967484be1cb0050e
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-06-27 23:03:56 +0530

    commit

--- dsStack.js
+++ dsStack.js
@@ -3,20 +3,3 @@
 //stack of plates
 //Last plate inserted and also the first plates removed
 //push,pop
-class stack{
-    stack = []
-    function addE(){
-        this.stack.push()
-    }
-    function deleteE{
-        this.stack.pop()
-    }
-    const display = ()=>{
-        console.log(this.stack)
-    }
-}
-
-const newStack = new Stack()
-newStack.addE(1)
-newStack.addE(2)
-newStack.display()
