da2831019d92dc6f719c4d762e859c7755598503
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-07-03 07:56:12 +0530

    commit

--- dsQueue.js
+++ dsQueue.js
@@ -1,18 +1,37 @@
-class Queue{
-    constructor(){
-        this.item = []
-    }
+class Queue {
+  constructor() {
+    this.items = [];
+  }
 
-    enqueue(element){
-        this.item.push(element)
-    }
+  enqueue(element) {
+    this.items.push(element);
+  }
 
-    dequeue(){
-        if(this.isEmpty()) return "Underflow"
-        return this.item.shift()
-    }
+  dequeue() {
+    if (this.isEmpty()) return "Underflow";
+    return this.items.shift();
+  }
 
-    front(){
-        
-    }
+  front() {
+    if (this.isEmpty()) return "No elements in Queue";
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  printQueue() {
+    let str = "";
+    for (let i = 0; i < this.items.length; i++) str += this.items[i] + " ";
+    return str;
+  }
 }
+
+const queue = new Queue();
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+console.log(queue.printQueue()); 
+console.log(queue.dequeue()); 
+console.log(queue.front());
